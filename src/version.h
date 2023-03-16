38a1a8d5cb16a67f50ded050dc6561474c9d7907
Magnus Norddahl <dpjudas@users.noreply.github.com>
2023-03-16 21:12:29 +0100

    Rename the project

--- src/version.h
+++ src/version.h
@@ -41,20 +41,26 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.11pre"
+#define VERSIONSTR "1.0pre"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,10,9999,0
-#define RC_PRODUCTVERSION 4,10,9999,0
+#define RC_FILEVERSION 1,0,9999,0
+#define RC_PRODUCTVERSION 1,0,9999,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
+
 // These are for content versioning.
-#define VER_MAJOR 4
-#define VER_MINOR 11
+#define VER_MAJOR 1
+#define VER_MINOR 0
 #define VER_REVISION 0
 
-// This should always refer to the GZDoom version a derived port is based on and not reflect the derived port's version number!
-#define ENG_MAJOR 4
-#define ENG_MINOR 11
+// These are for zscript versioning.
+#define ZSCRIPT_VER_MAJOR 4
+#define ZSCRIPT_VER_MINOR 11
+#define ZSCRIPT_VER_REVISION 0
+
+// This should always refer to the VkDoom version a derived port is based on and not reflect the derived port's version number!
+#define ENG_MAJOR 1
+#define ENG_MINOR 0
 #define ENG_REVISION 0
 
 // Version identifier for network games.
@@ -91,18 +97,18 @@ const char *GetVersionString();
 #define SAVEVER 4560
 
 // This is so that derivates can use the same savegame versions without worrying about engine compatibility
-#define GAMESIG "GZDOOM"
-#define BASEWAD "gzdoom.pk3"
+#define GAMESIG "VKDOOM"
+#define BASEWAD "vkdoom.pk3"
 #define OPTIONALWAD "game_support.pk3"
 #define GZDOOM 1
 #define VR3D_ENABLED
 
 // More stuff that needs to be different for derivatives.
-#define GAMENAME "GZDoom"
-#define WGAMENAME L"GZDoom"
-#define GAMENAMELOWERCASE "gzdoom"
-#define FORUM_URL "http://forum.zdoom.org/"
-#define BUGS_FORUM_URL	"http://forum.zdoom.org/viewforum.php?f=2"
+#define GAMENAME "VKDoom"
+#define WGAMENAME L"VKDoom"
+#define GAMENAMELOWERCASE "vkdoom"
+//#define FORUM_URL "http://forum.zdoom.org/"
+//#define BUGS_FORUM_URL	"http://forum.zdoom.org/viewforum.php?f=2"
 
 #if defined(__APPLE__) || defined(_WIN32)
 #define GAME_DIR GAMENAME
