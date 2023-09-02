0835ae415b40aa7f2fbe803fce58d8623c1ed1c9
Magnus Norddahl <dpjudas@users.noreply.github.com>
2023-09-02 15:14:01 +0200

    Use the command buffers from VkCommandBufferManager

--- src/common/rendering/vulkan/vk_renderdevice.cpp
+++ src/common/rendering/vulkan/vk_renderdevice.cpp
@@ -549,7 +549,6 @@ void VulkanRenderDevice::SetLevelMesh(LevelMesh* mesh)
 
 		Printf("Running VkLightmap.\n");
 
-		GetCommands()->WaitForCommands(false);
 		GetLightmap()->Raytrace(mesh);
 
 		Printf("Copying data.\n");
