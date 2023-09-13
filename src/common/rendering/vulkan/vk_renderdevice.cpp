c6b92e1c1b7c21f0a1092841564d500f49b83085
RaveYard <29225776+MrRaveYard@users.noreply.github.com>
2023-09-13 18:08:54 +0200

    Initial implementation for lightmap lump loading

--- src/common/rendering/vulkan/vk_renderdevice.cpp
+++ src/common/rendering/vulkan/vk_renderdevice.cpp
@@ -473,7 +473,7 @@ void VulkanRenderDevice::BeginFrame()
 		if (levelMesh && levelMesh->GetSurfaceCount() > 0)
 		{
 			levelMesh->UpdateLightLists();
-			GetTextureManager()->CreateLightmap(levelMesh->LMTextureSize, levelMesh->LMTextureCount);
+			GetTextureManager()->CreateLightmap(levelMesh->LMTextureSize, levelMesh->LMTextureCount, std::move(levelMesh->LMTextureData));
 			GetLightmap()->SetLevelMesh(levelMesh);
 
 #if 0 // full lightmap generation
