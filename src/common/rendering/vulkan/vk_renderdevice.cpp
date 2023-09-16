2178de0c6c440c351248dd7b908eb2ed22d3fb08
Magnus Norddahl <dpjudas@users.noreply.github.com>
2023-09-16 05:12:31 +0200

    - Fix VkRaytrace::Raytrace not working properly if called multiple times per frame. - Only call VkRaytrace::Raytrace once per scene

--- src/common/rendering/vulkan/vk_renderdevice.cpp
+++ src/common/rendering/vulkan/vk_renderdevice.cpp
@@ -486,6 +486,7 @@ void VulkanRenderDevice::BeginFrame()
 	for (auto& renderstate : mRenderState)
 		renderstate->BeginFrame();
 	mDescriptorSetManager->BeginFrame();
+	mLightmap->BeginFrame();
 }
 
 void VulkanRenderDevice::Draw2D()
