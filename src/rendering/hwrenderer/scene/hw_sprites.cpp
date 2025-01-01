6f425cf523fb6d7b8b4e8a5132d76a958052662e
Boondorl <boondorl@live.com>
2025-01-01 06:07:35 -0500

    Updated fogball sorting
    Take sector portals into account.

--- src/rendering/hwrenderer/scene/hw_sprites.cpp
+++ src/rendering/hwrenderer/scene/hw_sprites.cpp
@@ -833,7 +833,7 @@ void HWSprite::Process(HWDrawInfo *di, FRenderState& state, AActor* thing, secto
 	if (isFogball)
 	{
 		Fogball fogball;
-		fogball.Position = FVector3(thing->Pos());
+		fogball.Position = FVector3(thing->PosRelative(r_viewpoint.sector));
 		fogball.Radius = (float)thing->args[3];
 		fogball.Color = FVector3(powf(thing->args[0] * (1.0f / 255.0f), 2.2), powf(thing->args[1] * (1.0f / 255.0f), 2.2), powf(thing->args[2] * (1.0f / 255.0f), 2.2));
 		fogball.Fog = (float)thing->Alpha;
