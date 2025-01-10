71cafec63bc48e35fe545b530ef4987f8ac29e40
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2025-01-10 05:21:53 -0500

    - allow defcvars from pwad files

--- src/d_defcvars.cpp
+++ src/d_defcvars.cpp
@@ -45,6 +45,8 @@ void D_GrabCVarDefaults()
 
 	while ((lump = fileSystem.FindLump("DEFCVARS", &lastlump)) != -1)
 	{
+#if 0
+// what a silly restriction from gzdoom, thankfully we don't have to adhere to it in vkdoom!
 		// don't parse from wads
 		if (lastlump > fileSystem.GetLastEntry(fileSystem.GetMaxIwadNum()))
 		{
@@ -52,6 +54,7 @@ void D_GrabCVarDefaults()
 			Printf(TEXTCOLOR_RED "Cannot load DEFCVARS from a wadfile!\n");
 			break;
 		}
+#endif
 
 		FScanner sc(lump);
 
