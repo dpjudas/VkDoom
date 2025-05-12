27f0bea2bc64cfe68430c7a7a6b8f9c8ccdce8e2
RaveYard <29225776+MrRaveYard@users.noreply.github.com>
2025-05-13 01:20:48 +0200

    Prevent levelmesh updates when ceiling/floor is moving at a zero speed

--- src/playsim/p_map.cpp
+++ src/playsim/p_map.cpp
@@ -7191,18 +7191,23 @@ bool P_ChangeSector(sector_t *sector, int crunch, double amt, int floorOrCeil, b
 		}
 
 	}
-	switch(floorOrCeil)
-	{	// update levelmesh
-	case 0:
-		LevelMeshUpdater->FloorHeightChanged(sector);
-		break;
-	case 1:
-		LevelMeshUpdater->CeilingHeightChanged(sector);
-		break;
-	case 2:
-		LevelMeshUpdater->MidTex3DHeightChanged(sector);
-		break;
+
+	if (amt != 0.0)
+	{
+		switch(floorOrCeil)
+		{	// update levelmesh
+		case 0:
+			LevelMeshUpdater->FloorHeightChanged(sector);
+			break;
+		case 1:
+			LevelMeshUpdater->CeilingHeightChanged(sector);
+			break;
+		case 2:
+			LevelMeshUpdater->MidTex3DHeightChanged(sector);
+			break;
+		}
 	}
+
 	return cpos.nofit;
 }
 
