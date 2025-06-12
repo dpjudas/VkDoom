814ef6370ed1a946653047cee83def356b747eff
nashmuhandes <nashbackslash@gmail.com>
2025-06-13 03:52:36 +0800

    Add missing namespace checks

--- src/maploader/udmf.cpp
+++ src/maploader/udmf.cpp
@@ -845,10 +845,12 @@ public:
 				break;
 
 			case NAME_lm_bounce:
+				CHECK_N(Zd | Zdt)
 				Level->LightBounce = CheckBool(key);
 				break;
 
 			case NAME_lm_ao:
+				CHECK_N(Zd | Zdt)
 				Level->AmbientOcclusion = CheckBool(key);
 				break;
 
@@ -2308,6 +2310,7 @@ public:
 					break;
 
 				case NAME_lm_dynamic:
+					CHECK_N(Zd | Zdt)
 					Flag(sec->Flags, SECF_LM_DYNAMIC, key);
 					continue;
 
